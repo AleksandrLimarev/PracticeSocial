e9ba978159ab0ef5bd115a73be4ed5e849fa8294
Aleksandr Limarev <limarev.sanya@gmail.com>
2022-10-23 02:13:27 +0200

    Initial Commit

---
+++ PracticeSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeSocial
+//
+//  Created by Александр Лимарев on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
